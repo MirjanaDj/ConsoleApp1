867eb2a0376c1c32ed8af2a32fd382dbbcfb855b
MirjanaDj <mirjana.jovanovic.sd@gmail.com>
2018-07-15 01:19:48 +0200

    Dodat je folder CSharp

---
+++ ConsoleApp1/MatematickeFunkcije.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatFunc
+{
+    public class matematickeFunkcije
+    {
+        public int Saberi(int a1, int a2)
+        {
+            return a1 + a2;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using MatFunc;
+using System;
+
+namespace Mira
+{
+    class Perta
+    {
+        static void Main(string[] args)
+        {
+            //    Console.WriteLine("Zdravo svete");
+            //    matematickeFunkcije mtt = new matematickeFunkcije();
+            //    string rezultat = Convert.ToString(mtt.Saberi(1, 4));
+            //    Console.WriteLine(rezultat);
+            //    rezultat = rezultat + mtt.Saberi(2, 4);
+            //    Console.WriteLine(rezultat);
+
+            //    bool tt = false;
+            //    Console.WriteLine(tt);
+
+            //int a = 10;
+            //int b = 20;
+
+            //int c = a + b;
+
+            //Console.WriteLine("Kada sam sa {0} sabrao {1} dobio sam {2}",a,b,c);
+
+            const double pi = 3.14159;
+
+            double r;
+
+            Console.WriteLine("Unesite radijus:");
+
+            string karakter = Console.ReadLine();
+
+            r = Convert.ToDouble(karakter);
+        }
+    }
+}
